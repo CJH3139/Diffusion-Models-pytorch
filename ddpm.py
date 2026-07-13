2c9954ef265995d7fe738e076c7046aa02ff3314
CJH3139 <imnotnotcjh@gmail.com>
2026-07-12 21:14:22 -0700

    reduces step from 1000 to 900

--- ddpm.py
+++ ddpm.py
@@ -1,4 +1,4 @@
-Optimport os
+Optwhereimport os
 import torch
 import torch.nn as nn
 from matplotlib import pyplot as plt
@@ -13,7 +13,7 @@ logging.basicConfig(format="%(asctime)s - %(levelname)s: %(message)s", level=log
 
 
 class Diffusion:
-    def __init__(self, noise_steps=1000, beta_start=1e-4, beta_end=0.02, img_size=256, device="cuda"):
+    def __init__(self, noise_steps=900, beta_start=1e-4, beta_end=0.02, img_size=256, device="cuda"):
         self.noise_steps = noise_steps
         self.beta_start = beta_start
         self.beta_end = beta_end
@@ -41,12 +41,14 @@ class Diffusion:
         model.eval()
         with torch.no_grad():
             x = torch.randn((n, 3, self.img_size, self.img_size)).to(self.device)
+            beta_record = []
             for i in tqdm(reversed(range(1, self.noise_steps)), position=0):
                 t = (torch.ones(n) * i).long().to(self.device)
                 predicted_noise = model(x, t)
                 alpha = self.alpha[t][:, None, None, None]
                 alpha_hat = self.alpha_hat[t][:, None, None, None]
                 beta = self.beta[t][:, None, None, None]
+                beta_record = [beta_record,beta]
                 if i > 1:
                     noise = torch.randn_like(x)
                 else:
